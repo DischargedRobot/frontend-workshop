34452dc42887ed368612b4836f38aca1db1c82fa
DischargedRobot <kolyaportov@yandex.ru>
2026-03-11 18:17:11 +0300

    add: UserSeach

---
+++ features/UserSearch/UserSearch.tsx
@@ -0,0 +1,22 @@
+import { Input } from "antd"
+import React from "react"
+
+const { Search } = Input
+
+interface Props {
+    onSearch: React.ChangeEventHandler<HTMLInputElement>
+}
+
+
+const UserSearch = (props: Props) => {
+
+    const {
+        onSearch,
+    } = props
+
+    return(
+        <Search type={'search'} enterButton onChange={onSearch} placeholder="Имя фич флага"/>
+    )
+}
+
+export default UserSearch
