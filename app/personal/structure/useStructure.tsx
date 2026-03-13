f584e9bd716c67ee662c371b0a258e28b5796995
DischargedRobot <kolyaportov@yandex.ru>
2026-03-13 21:32:00 +0300

    add: a hook useStructure  for a structure page

--- app/personal/structure/useStructure.tsx
+++ app/personal/structure/useStructure.tsx
@@ -1,11 +1,17 @@
+import { IDepartment } from "@/entities/DepartmentTable";
 import { IUser } from "@/entities/UserCard/types";
-import { set } from "react-hook-form";
 import { create } from "zustand";
 
+const API_URL = 'api/'
 
 interface IUseStructure {
     users: IUser[]
-    setUsers: (users: IUser[]) => void
+    setUsers: (newUsers: IUser[]) => void
+
+    departments: IDepartment[]
+    setDepartments: (newDepartments: IDepartment[]) => void
+
+    getDepartments: () => void
 }
 
 const useStructure = create<IUseStructure>((set, get) => ({
@@ -13,4 +19,26 @@ const useStructure = create<IUseStructure>((set, get) => ({
     users: [],
     setUsers: (items) => set({users: items}),
 
+    departments: [],
+    setDepartments: (items) => set({departments: items}),
+
+    getDepartments: async () => {
+        const response = await fetch('api/getDepartments')
+        
+        if (!response.ok) {
+            throw new Error('Ошибка получения отделов')
+        }
+
+        return await response.json()
+    },
+
+    getUsersByDepartments: async () => {
+        const response = await fetch(API_URL)
+
+        if (!response.ok){
+            throw new Error('bee users')
+        }
+
+        return await response.json()
+    }
 }))
