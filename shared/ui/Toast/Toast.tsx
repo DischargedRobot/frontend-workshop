493ea724b889847bd5df15192f6e227c28ded201
DischargedRobot <kolyaportov@yandex.ru>
2026-04-13 17:04:32 +0300

    add: toastLoader

--- shared/ui/Toast/Toast.tsx
+++ shared/ui/Toast/Toast.tsx
@@ -9,18 +9,21 @@ import {
 	WarningOutlined,
 } from "@ant-design/icons"
 import { create } from "zustand"
+import { Spin } from "antd"
 
-type TToast = "warning" | "success" | "error"
+type TToast = "warning" | "success" | "error" | "load"
 const defaultTitle = new Map<TToast, string>([
 	["warning", "Предупреждение"],
 	["success", "Успех"],
 	["error", "Ошибка"],
+	["load", "Загрузка"]
 ])
 
 const icons = new Map<TToast, React.ReactNode>([
 	["warning", <WarningOutlined className="toast__icon" key={"warning"} />],
 	["success", <CheckOutlined className="toast__icon" key={"success"} />],
 	["error", <ExclamationOutlined className="toast__icon" key={"error"} />],
+	["load", <Spin className="toas__icon" key={"load"} />]
 ])
 
 interface IToast {
@@ -139,7 +142,53 @@ const Toast = ({
 export default Toast
 
 export const showToast = (props: IToast) => {
-	useToastStore.getState().addToast(props)
+	return useToastStore.getState().addToast(props)
+}
+
+// export const changeShowingToas = (toast: IToast, toastId: number) => {
+// 	const changeToast = useToastStore.getState().changeToast
+
+// 	return
+// }
+
+/**
+ * @desc Показ тоста с лоэдером
+ * @param initialStateToast - изначальнео состояние тоста
+ * @returns колбек функция для изменения состояния, принимающая новое состояние
+ */
+export const showLoadToast = (initialStateToast: IToast) => {
+	const addToast = useToastStore.getState().addToast
+	const changeToast = useToastStore.getState().changeToast
+
+	const changedToastId = addToast(initialStateToast)
+
+	return (newStateToast: IToast) => {
+		const changedToast = useToastStore.getState().getToastById(changedToastId)
+		changeToast(newStateToast, changedToastId)
+	}
+}
+
+export const showLoadToastByDuration = (toasts: IToast[]) => {
+	const removeToast = useToastStore.getState().removeToast
+	const addToast = useToastStore.getState().addToast
+	const changeToast = useToastStore.getState().changeToast
+
+	const changedToastId = addToast(toasts[0])
+	let currentToastNumber = 0
+
+	const changeToastLoadByDuration = () => {
+		setTimeout(() => {
+			changeToast(toasts[currentToastNumber++], changedToastId)
+			if (toasts[currentToastNumber] !== undefined) {
+				changeToastLoadByDuration()
+			}
+		},
+			toasts[currentToastNumber].duration)
+	}
+
+	changeToastLoadByDuration()
+
+	removeToast(changedToastId)
 }
 
 interface IToastStore {
@@ -149,19 +198,28 @@ interface IToastStore {
 		position: { top: number; left: number }
 		activatedAt: number
 	} | null
-	addToast: (toast: IToast) => void
+	getToastById: (id: number) => IToastItem | null
+	addToast: (toast: IToast) => number
 	removeToast: (id: number) => void
 	setActiveToast: (
 		toast: { id: number; position: { top: number; left: number } } | null,
 	) => void
+
+	changeToast: (toast: IToast, id: number) => void
 }
 
-export const useToastStore = create<IToastStore>((set) => {
+export const useToastStore = create<IToastStore>((set, get) => {
 	let toastId = 0
 	return {
 		toasts: [],
 		activeToast: null,
-		addToast: (newToast) =>
+
+		getToastById: (id: number) => {
+			return get().toasts.find(toast => toast.id === id) ?? null
+		},
+
+
+		addToast: (newToast) => {
 			set((state) => ({
 				toasts: [
 					...state.toasts,
@@ -173,16 +231,32 @@ export const useToastStore = create<IToastStore>((set) => {
 						id: ++toastId,
 					},
 				],
-			})),
+			}))
+			return toastId
+		},
+
 		removeToast: (id) =>
 			set((state) => ({
 				toasts: state.toasts.filter((toast) => toast.id !== id),
 			})),
+
 		setActiveToast: (activeToast) =>
 			set({
 				activeToast: activeToast
 					? { ...activeToast, activatedAt: Date.now() }
 					: null,
 			}),
+
+		changeToast: (newToast, id) => set(state => {
+			const newToasts = state.toasts.map(toast => {
+				if (toast.id === id) {
+					return { ...newToast, id: id }
+				}
+
+				return toast
+			})
+
+			return { toasts: newToasts }
+		})
 	}
 })
